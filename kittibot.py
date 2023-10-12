3899dc124ece24a5d6d331a5ea366e04a861434c
myasn1kov <myasn1kov@mail.ru>
2023-10-12 18:52:50 +0300

    secure

--- kittibot.py
+++ kittibot.py
@@ -16,61 +16,67 @@ URL = 'https://api.thecatapi.com/v1/images/search'
 chat_id = 533401501
 text = 'Я пока совсем глупый!'
 # Отправка сообщения
-#bot.send_message(chat_id, text) 
+# bot.send_message(chat_id, text)
+
 
 def get_new_image():
     try:
-        response = requests.get(URL) # пробуем получить фото котика 
-    except Exception as error: # обрабатываем исключение и заменяем url
+        response = requests.get(URL)  # пробуем получить фото котика
+    except Exception as error:  # обрабатываем исключение и заменяем url
         # Печатать информацию в консоль теперь не нужно:
         # всё необходимое будет в логах
-        # print(error)      
+        # print(error)
         logging.error(f'Ошибка при запросе к основному API: {error}')
         new_url = 'https://api.thedogapi.com/v1/images/search'
         response = requests.get(new_url)
-    
+
     response = response.json()
     random_cat = response[0].get('url')
-    return random_cat 
+    return random_cat
+
 
 def new_cat(update, context):
     chat = update.effective_chat
     context.bot.send_photo(chat.id, get_new_image())
 
+
 def wake_up(update, context):
-    # В ответ на команду /start 
+    # В ответ на команду /start
     # будет отправлено сообщение 'Спасибо, что включили меня'
     chat = update.effective_chat
     name = update.message.chat.first_name
     # Вот она, наша кнопка.
-    # Обратите внимание: в класс передаётся список, вложенный в список, 
+    # Обратите внимание: в класс передаётся список, вложенный в список,
     # даже если кнопка всего одна.
     # Каждый вложенный список определяет
     # новый ряд кнопок в интерфейсе бота.
     # Здесь описаны две кнопки в первом ряду и одна - во втором.
     buttons = ReplyKeyboardMarkup(
-        [['Обедал ли Саша?', 'Статус Саши'],['Фото котика', '/newcat']], resize_keyboard=True)
-    context.bot.send_message(chat_id=chat.id, 
-                             text='Спасибо, что включили меня, {}!'.format(name), 
+        [['Обедал ли Саша?', 'Статус Саши'], ['Фото котика', '/newcat']], resize_keyboard=True)
+    context.bot.send_message(chat_id=chat.id,
+                             text='Спасибо, что включили меня, {}!'.format(
+                                 name),
                              # Добавим кнопку в содержимое отправляемого сообщения
                              reply_markup=buttons)
 
+
 def status(update, context):
-    # В ответ на команду /status 
+    # В ответ на команду /status
     # будет отправлено сообщение 'Саша вас очень любит!'
     chat = update.effective_chat
     context.bot.send_message(chat_id=chat.id,
                              text='Саша вас очень любит!')
 
+
 def dinner(update, context):
-    # В ответ на команду /status 
+    # В ответ на команду /status
     # будет отправлено сообщение 'Саша вас очень любит!'
     chat = update.effective_chat
     if datetime.now().time() < datetime.strptime('14:00', '%H:%M').time():
-        context.bot.send_message(chat_id=chat.id, 
+        context.bot.send_message(chat_id=chat.id,
                                  text='Саша еще не обедал!')
     else:
-        context.bot.send_message(chat_id=chat.id, 
+        context.bot.send_message(chat_id=chat.id,
                                  text='Саша уже пообедал!')
 
 
@@ -78,16 +84,18 @@ def say_hi(update, context):
     # Получаем информацию о чате, из которого пришло сообщение,
     # и сохраняем в переменную chat
     chat = update.effective_chat
-    # В ответ на любое текстовое сообщение 
+    # В ответ на любое текстовое сообщение
     # будет отправлено 'Привет, я KittyBot!'
-    context.bot.send_message(chat_id=chat.id, text='Саша получил ваше сообщение!')
+    context.bot.send_message(
+        chat_id=chat.id, text='Саша получил ваше сообщение!')
 
 # Регистрируется обработчик CommandHandler;
 # он будет отфильтровывать только сообщения с содержимым '/start'
 # и передавать их в функцию wake_up()
 
+
 def main():
-    # Здесь укажите токен, 
+    # Здесь укажите токен,
     # который вы получили от @Botfather при создании бот-аккаунта
     bot = Bot(token=secret_token)
     updater = Updater(token=secret_token)
@@ -96,17 +104,20 @@ def main():
     # Регистрируется обработчик MessageHandler;
     # из всех полученных сообщений он будет выбирать только текстовые сообщения
     # и передавать их в функцию say_hi() или status
-    updater.dispatcher.add_handler(MessageHandler(Filters.text('Статус Саши'), status))
-    updater.dispatcher.add_handler(MessageHandler(Filters.text('Обедал ли Саша?'), dinner))
-    updater.dispatcher.add_handler(MessageHandler(Filters.text('Фото котика'), new_cat))
+    updater.dispatcher.add_handler(
+        MessageHandler(Filters.text('Статус Саши'), status))
+    updater.dispatcher.add_handler(MessageHandler(
+        Filters.text('Обедал ли Саша?'), dinner))
+    updater.dispatcher.add_handler(MessageHandler(
+        Filters.text('Фото котика'), new_cat))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
 
-
-    # Метод start_polling() запускает процесс polling, 
+    # Метод start_polling() запускает процесс polling,
     # приложение начнёт отправлять регулярные запросы для получения обновлений.
     updater.start_polling()
     # Бот будет работать до тех пор, пока не нажмете Ctrl-C
-    updater.idle() 
+    updater.idle()
+
 
 if __name__ == '__main__':
-    main() 
+    main()
--- test_work_with_secrets.py
+++ test_work_with_secrets.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 # Теперь переменная TOKEN, описанная в файле .env,
-# доступна в пространстве переменных окружения 
+# доступна в пространстве переменных окружения
 
 token = os.getenv('TOKEN')
-print(token)  # 1896714192:AAFmXXXXPEn6KqewJ13kKKtlnBYqVC_XXXx 
+print(token)  # токен
